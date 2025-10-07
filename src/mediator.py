5996b4a90beb926444bb1911d3e3a31258baa423
Giovanna Alves Barbosa de Oliveira <164058997+GIOV4NN4EC@users.noreply.github.com>
2025-10-06 21:43:32 -0300

    behavioral pattern: mediator

---
+++ src/mediator.py
@@ -0,0 +1,51 @@
+from src.classes import Pet, Adopter, Shelter, Application
+
+class AdoptionMediator:
+    def create_application(self, applicant: str, pet: str, answers: list[str]) -> Application:
+        raise NotImplementedError
+    
+    def approve_application(self, application: Application) -> None:
+        raise NotImplementedError
+    
+    def deny_application(self, application: Application, feedback: str) -> None:
+        raise NotImplementedError
+
+
+class ConcreteAdoptionMediator(AdoptionMediator):
+    def __init__(self):
+        self.pets = Pet.data
+        self.adopters = Adopter.data
+        self.shelters = Shelter.data
+        self.applications = Application.data
+
+    def create_application(self, applicant: str, pet: str, answers: list[str]):
+        pet_obj = self.pets.get(pet)
+        adopter = self.adopters.get(applicant)
+        
+        if not pet_obj or not adopter:
+            raise ValueError("Pet or adopter not found!")
+        
+        # creating the application
+        app = Application(applicant, pet, pet_obj.form, answers)
+
+        # updating pet
+        pet_obj.add_application()
+        
+        return app
+    
+    def approve_application(self, application: Application):
+        pet_obj = self.pets.get(application.pet)
+        adopter = self.adopters.get(application.applicant)
+
+        if not pet_obj or not adopter:
+            raise ValueError("Pet or adopter not found")
+        
+        application._set_status("approved")
+        pet_obj.tutor = adopter
+        pet_obj.was_adopted()
+
+
+
+    def deny_application(self, application: Application, feedback: str = "") -> None:
+        application._set_status("denied")
+        application.feedback = feedback
