4ec77d4ee9f2bd2a40d1856f6390bbe83e769a8e
Giovanna Alves Barbosa de Oliveira <164058997+GIOV4NN4EC@users.noreply.github.com>
2025-11-05 00:00:03 -0300

    refactor: errors for handling empty and invalid names

--- src/ui/name_validator.py
+++ src/ui/name_validator.py
@@ -1,10 +1,21 @@
 from questionary import Validator, ValidationError
+from src.exceptions import InvalidNameError, EmptyFieldError
 
 
 class NameValidator(Validator):
     def validate(self, document):
-        if len(document.text) == 0:
+        text = document.text.strip()
+
+        # EmptyFieldError
+        if not text:
             raise ValidationError(
-                message="Please enter a value",
+                message=str(EmptyFieldError("Please enter a value")),
+                cursor_position=len(document.text),
+            )
+
+        # InvalidNameError
+        if not all(char.isalpha() or char.isspace() for char in text):
+            raise ValidationError(
+                message=str(InvalidNameError("Name can only contain letters")),
                 cursor_position=len(document.text),
             )
